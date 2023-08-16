c985b36a786a440965116a830ff8f153de82ae5f
HENZO FELIPE CARVALHO DE MATTOS <B51104@fgv.edu.br>
2023-08-16 08:54:57 -0300

    fifth corrected

--- five.py
+++ five.py
@@ -1,19 +1,22 @@
 import numpy as np
-import numpy.random as npr
 
 def task_5(nda_five, nda_six):
-    nda_seven = np.hstack(nda_five, nda_six)
+    nda_seven = np.hstack((nda_five, nda_six))
     
     media = nda_seven.mean()
-    desv_pad = nda_seven.sdv()
+    desv_pad = nda_seven.std()
     var = nda_seven.var()
     
-    print("Média: %d\nDesvio Padrão: %d\nVariância: %d", media, desv_pad, var)
+    print(f"Média: {media}\nDesvio Padrão: {desv_pad}\nVariância: {var}\n")
     
-    arr_size = nda_seven.shape()
-    odd_num = np.arange(1, arr_size[0], 2)
-    even_num = np.aragen(0, arr_size[0], 2)
+    nda_size = np.shape(nda_seven)
+    odd_num = np.arange(1, nda_size[0], 2)
+    even_num = np.arange(0, nda_size[0], 2)
     
-    nda_seven(odd_num) = -1
-    nda_seven(even_num) = 1
-    
+    for i in range(0, nda_size[0]):
+        if (nda_seven[i] % 2 == 0):
+            nda_seven[i] = 1
+        else:
+            nda_seven[i] = -1
+            
+    print(nda_seven)
