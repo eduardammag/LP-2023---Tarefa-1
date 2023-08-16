a99260f41a257002fcd97c1e08daaaba790cbb2f
MARIA EDUARDA MESQUITA MAGALHÃES <B51085@fgv.edu.br>
2023-08-16 08:11:19 -0300

    Geração da questão 1

---
+++ generation.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+import numpy.random as npr
+
+#1
+
+a1D_1= np.arange(5,32,3)
+print(a1D_1)
+
+a1D_2 = npr.randint(3,77,9)
+print(a1D_2)
+
+a1D_soma = a1D_1 + a1D_2
+print(a1D_soma)
